c7c1ea5e990039887415bfb0ed21f3befb355548
Artem <dalyvinchi@gmail.com>
2025-05-30 00:06:29 +0300

    UPD Update models

--- app/models/user.py
+++ app/models/user.py
@@ -4,3 +4,15 @@ class UserModel(Document):
     username: str
     first_name: str
     last_name: str
+    
+    class Settings:
+        name = "users"
+        
+    class Config:
+        schema_extra = {
+            "example": {
+                "username": "JohnDoe123",
+                "first_name": "John",
+                "last_name": "Doe"
+            }
+        }
